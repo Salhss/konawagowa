76c295c86ae867e984f646bb0233f52fce0dc7db
OMO-git <omogit3@gmail.com>
2024-02-10 22:40:09 +0700

    refactor: responsive image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,11 +13,31 @@ import '../assets/colors.css'
 
 <style scoped>
 main {
-  height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 26px;
+    padding-inline: 15px;
+    height: 100vh;
   background-color: var(--red-default);
+  }
+  .hero-banner {
+    width: 90%;
+    height: 400px;
+  }
+  .hero-image {
+    object-fit: cover;
+    object-position: center;
+    border-radius: 20px;
+    width: 100%;
+    height: 100%;
+  }
+
+@media (min-width: 913px) {
+  .hero-banner {
+    width: 70%;
+  }
 }
-
-
 
 @media (max-width: 912px) {
   main {
@@ -28,8 +48,8 @@ main {
     padding-inline: 15px;
   }
   .hero-banner {
-    width: 100%;
-    height: 242px;
+    width: 90%;
+    height: 400px;
   }
   .hero-image {
     object-fit: cover;
@@ -38,6 +58,12 @@ main {
     width: 100%;
     height: 100%;
   }
-  
+}
+
+@media (max-width: 430px) {
+  .hero-banner {
+    width: 100%;
+    height: 242px;
+  }
 }
 </style>
