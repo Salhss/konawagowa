f1eff7aa278301fe79c30a542c561626636dbd1d
OMO-git <omogit3@gmail.com>
2024-02-13 02:26:45 +0700

    refactor: benefit section responsive

--- src/components/home/Benefit.vue
+++ src/components/home/Benefit.vue
@@ -8,68 +8,88 @@ export default {
   <h1 class="benefit-head">Beragam Keseruan<br />Yang Bisa Kamu Dapatkan</h1>
   <div class="image-background">
     <div class="image-container">
-        <img
-          src="@/assets/images/benefit-image.jpeg"
-          alt="poundfit konawa"
-          class="benefit-img"
-        />
+      <img
+        src="@/assets/images/benefit-image.jpeg"
+        alt="poundfit konawa"
+        class="benefit-img"
+      />
     </div>
   </div>
   <div class="benefits-container">
     <div class="benefits-left">
       <div class="benefit-item">
         <div class="logo-container">
-          <img src="../../assets/images/poundfit-black.png" alt="konawa poundfit" class="benefit-logo poundfit"/>
+          <img
+            src="../../assets/images/poundfit-black.png"
+            alt="konawa poundfit"
+            class="benefit-logo poundfit"
+          />
         </div>
         <p>Senam Poundfit</p>
       </div>
       <div class="benefit-item">
         <div class="logo-container">
-          <img src="../../assets/images/zumba-logo.png" alt="konawa zumba" class="zumba"/>
+          <img
+            src="../../assets/images/zumba-logo.png"
+            alt="konawa zumba"
+            class="zumba"
+          />
         </div>
         <p>Senam Zumba</p>
       </div>
       <div class="benefit-item">
         <div class="logo-container">
-          <svg class="community" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" viewBox="0 0 502.648 502.648" xml:space="preserve" fill="#000000">
-
-<g id="SVGRepo_bgCarrier" stroke-width="0"/>
-
-<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-
-<g id="SVGRepo_iconCarrier"> <g> <g> <g> <circle style="fill:#453235;" cx="250.399" cy="91.549" r="58.694"/> <path style="fill:#453235;" d="M455.861,253.028l-54.703-11.411c-18.637-3.904-37.037,4.638-46.765,19.824 c-9.448-4.853-19.608-9.038-30.415-12.511v-32.529c0.022-24.612-20.126-44.738-44.651-44.738h-55.933 c-24.655,0-44.716,20.126-44.716,44.738v32.701c-10.699,3.408-20.751,7.593-30.264,12.468 c-9.728-15.251-28.15-23.857-46.809-19.953l-54.747,11.411c-24.03,5.026-39.626,28.862-34.6,52.978l13.741,65.64 c4.983,24.051,28.84,39.647,52.892,34.621l17.321-3.624c8.671,12.813,20.665,24.569,36.023,34.621 c31.989,20.967,74.247,32.529,119.092,32.529c68.617,0,127.721-27.589,154.943-67.215l17.602,3.689 c24.03,5.004,47.887-10.57,52.87-34.621l13.762-65.64C495.508,281.89,479.912,258.054,455.861,253.028z M251.305,447.381 c-40.51,0-78.475-10.203-106.797-28.862c-9.707-6.342-17.753-13.395-24.202-20.945l13.266-2.783 c24.073-5.004,39.669-28.84,34.643-52.913l-12.317-59.018c7.183-3.861,14.733-7.248,22.757-10.138v10.764 c0,24.569,20.104,44.695,44.716,44.695h55.933c24.548,0,44.652-20.147,44.652-44.695v-11.325 c8.175,2.912,15.854,6.256,22.973,10.052L334.439,341.9c-4.983,24.073,10.591,47.909,34.664,52.913l13.395,2.804 C357.52,427.191,308.101,447.381,251.305,447.381z"/> <circle style="fill:#453235;" cx="443.954" cy="168.708" r="58.694"/> <path style="fill:#453235;" d="M70.736,226.172c31.752-6.644,52.029-37.77,45.471-69.501 c-6.687-31.709-37.749-52.072-69.523-45.428c-31.709,6.622-52.072,37.727-45.428,69.458 C7.879,212.453,38.984,232.795,70.736,226.172z"/> </g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g>
-
-</svg>
+          <img
+            src="../../assets/svgs/community.svg"
+            alt="komunitas seru"
+            loading="lazy"
+            class="community"
+          />
         </div>
         <p>Komunitas Seru</p>
+      </div>
+    </div>
+    <div class="middle-image-background">
+      <div class="image-container">
+        <img
+          src="@/assets/images/benefit-image.jpeg"
+          alt="poundfit konawa"
+          class="benefit-img"
+        />
       </div>
     </div>
     <div class="benefits-right">
       <div class="benefit-item">
         <div class="logo-container">
-          <img src="../../assets/images/poundfit-black.png" alt="konawa poundfit" class="benefit-logo poundfit"/>
+          <img
+            src="../../assets/svgs/competition.svg"
+            loading="lazy"
+            class="competition"
+          />
         </div>
-        <p>Senam Poundfit</p>
+        <p>Lomba Senam</p>
       </div>
       <div class="benefit-item">
         <div class="logo-container">
-          <img src="../../assets/images/zumba-logo.png" alt="konawa zumba" class="zumba"/>
+          <img
+            src="../../assets/svgs/dancing.svg"
+            alt="konawa aerobic"
+            loading="lazy"
+            class="aerobic"
+          />
         </div>
-        <p>Senam Zumba</p>
+        <p>Senam Aerobic</p>
       </div>
       <div class="benefit-item">
         <div class="logo-container">
-          <svg class="community" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" viewBox="0 0 502.648 502.648" xml:space="preserve" fill="#000000">
-
-<g id="SVGRepo_bgCarrier" stroke-width="0"/>
-
-<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-
-<g id="SVGRepo_iconCarrier"> <g> <g> <g> <circle style="fill:#453235;" cx="250.399" cy="91.549" r="58.694"/> <path style="fill:#453235;" d="M455.861,253.028l-54.703-11.411c-18.637-3.904-37.037,4.638-46.765,19.824 c-9.448-4.853-19.608-9.038-30.415-12.511v-32.529c0.022-24.612-20.126-44.738-44.651-44.738h-55.933 c-24.655,0-44.716,20.126-44.716,44.738v32.701c-10.699,3.408-20.751,7.593-30.264,12.468 c-9.728-15.251-28.15-23.857-46.809-19.953l-54.747,11.411c-24.03,5.026-39.626,28.862-34.6,52.978l13.741,65.64 c4.983,24.051,28.84,39.647,52.892,34.621l17.321-3.624c8.671,12.813,20.665,24.569,36.023,34.621 c31.989,20.967,74.247,32.529,119.092,32.529c68.617,0,127.721-27.589,154.943-67.215l17.602,3.689 c24.03,5.004,47.887-10.57,52.87-34.621l13.762-65.64C495.508,281.89,479.912,258.054,455.861,253.028z M251.305,447.381 c-40.51,0-78.475-10.203-106.797-28.862c-9.707-6.342-17.753-13.395-24.202-20.945l13.266-2.783 c24.073-5.004,39.669-28.84,34.643-52.913l-12.317-59.018c7.183-3.861,14.733-7.248,22.757-10.138v10.764 c0,24.569,20.104,44.695,44.716,44.695h55.933c24.548,0,44.652-20.147,44.652-44.695v-11.325 c8.175,2.912,15.854,6.256,22.973,10.052L334.439,341.9c-4.983,24.073,10.591,47.909,34.664,52.913l13.395,2.804 C357.52,427.191,308.101,447.381,251.305,447.381z"/> <circle style="fill:#453235;" cx="443.954" cy="168.708" r="58.694"/> <path style="fill:#453235;" d="M70.736,226.172c31.752-6.644,52.029-37.77,45.471-69.501 c-6.687-31.709-37.749-52.072-69.523-45.428c-31.709,6.622-52.072,37.727-45.428,69.458 C7.879,212.453,38.984,232.795,70.736,226.172z"/> </g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g>
-
-</svg>
+          <img
+            src="../../assets/svgs/holiday.svg"
+            alt="liburan bersama konawa"
+            loading="lazy"
+            class="holiday"
+          />
         </div>
-        <p>Komunitas Seru</p>
+        <p>Liburan Bersama</p>
       </div>
     </div>
   </div>
@@ -79,7 +99,7 @@ export default {
   text-align: center;
   color: var(--lighten-black);
 }
-.image-background {
+.image-background, .middle-image-background {
   background-image: url("../../assets/images/red-paint-bg.png");
   background-position: center;
   background-size: contain;
@@ -90,16 +110,17 @@ export default {
   align-items: center;
 }
 .benefit-img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 .benefits-container {
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
-.benefits-left, .benefits-right {
+.benefits-left,
+.benefits-right {
   display: flex;
   flex-direction: column;
 }
@@ -108,7 +129,7 @@ export default {
   align-items: center;
 }
 .benefit-item p {
-  color: #624F51;
+  color: #624f51;
 }
 .logo-container {
   background-color: rgba(210, 0, 26, 0.3);
@@ -119,26 +140,102 @@ export default {
 
 @media (min-width: 1024px) {
   .image-background {
-    background-size: cover;
-    width: 50%;
+    display: none;
   }
-  .image-container {
-    width: 400px;
-    margin-block: 24px;
+  .middle-image-background {
+    width: 500px;
   }
   .benefit-img {
+    width: 500px;
     border-radius: 20px;
+  }
+  .benefits-container {
+    justify-content: space-around;
+  }
+  .logo-container {
+    width: 75px;
+    height: 65px;
+    border-radius: 8px;
+  }
+  .benefits-right .benefit-item {
+    flex-direction: row-reverse;
+  }
+  .benefits-left, .benefits-right {
+    gap: 20px;
+  }
+  .benefit-item {
+    gap: 12px;
+  }
+  .benefit-item p {
+    font-size: 18px;
+  }
+  .poundfit {
+    width: 63px;
+  }
+  .zumba {
+    width: 68px;
+  }
+  .community {
+    width: 54px;
+  }
+  .competition {
+    width: 61px;
+  }
+  .aerobic {
+    width: 50.5px;
+  }
+  .holiday {
+    width: 54px;
   }
 }
 
 @media (min-width: 501px) and (max-width: 1023px) {
+  .middle-image-background {
+    display: none;
+  }
   .image-container {
     width: 500px;
-    height: 300px;
+    height: 350px;
     margin-block: 32px;
+  }
+  .benefits-container {
+    justify-content: space-around;
+  }
+  .benefits-left, .benefits-right {
+    gap: 20px;
+  }
+  .benefit-item {
+    gap: 12px;
+  }
+  .benefit-item p {
+    font-size: 20px;
+    font-weight: 600;
+  }
+  .logo-container {
+    width: 75px;
+    height: 65px;
+    border-radius: 8px;
   }
   .benefit-img {
     border-radius: 16px;
+  }
+  .poundfit {
+    width: 63px;
+  }
+  .zumba {
+    width: 68px;
+  }
+  .community {
+    width: 54px;
+  }
+  .competition {
+    width: 61px;
+  }
+  .aerobic {
+    width: 50.5px;
+  }
+  .holiday {
+    width: 54px;
   }
 }
 
@@ -148,6 +245,9 @@ export default {
     line-height: 1.4;
     margin-top: 40px;
     margin-bottom: 32px;
+  }
+  .middle-image-background {
+    display: none;
   }
   .image-background {
     margin-bottom: 28px;
@@ -163,7 +263,8 @@ export default {
   .benefits-container {
     padding-inline: 8px;
   }
-  .benefits-left, .benefits-right {
+  .benefits-left,
+  .benefits-right {
     gap: 28px;
   }
   .logo-container {
@@ -182,6 +283,15 @@ export default {
     width: 48px;
   }
   .community {
+    width: 34px;
+  }
+  .competition {
+    width: 41px;
+  }
+  .aerobic {
+    width: 30.5px;
+  }
+  .holiday {
     width: 34px;
   }
 }
